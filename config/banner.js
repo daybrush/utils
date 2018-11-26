a87472631926dbfb4319fb052800d78c851c1552
Daybrush <younkue.choi@navercorp.com>
2018-11-26 12:56:39 +0900

    fix(utils): remove all

--- config/banner.js
+++
@@ -1,8 +0,0 @@
-const pkg = require("../package.json");
-module.exports = `/*
-Copyright (c) 2018 ${pkg.author}
-license: ${pkg.license}
-author: ${pkg.author}
-repository: ${pkg.repository.url}
-@version ${pkg.version}
-*/`;
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./color";
-export * from "./consts";
-export * from "./utils";
-export * from "./css";
